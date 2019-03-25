84fa205ce18bcdb7fe5ed57d5f791002ce004011
Danny Brian <danny@brians.org>
2019-03-24 21:06:33 -0600

    not yet working, but testing

--- regex-highlighter.js
+++ regex-highlighter.js
@@ -1,6 +1,37 @@
 const regexpTree = require('regexp-tree');
- 
+
+//var regex1 = '/q(?!u)(\w+)[a-z]{1,}(?<hello>{{hello}})/i';
+
 // Get AST.
-const ast = regexpTree.parse('/[a-z]{1,}/');
+//const ast = regexpTree.parse(regex1, { captureLocations: true });
  
+//console.log(JSON.stringify(ast, null, '\t'));
+
+// regexHighlight: ({regex:String, type?:String}) => String
+
+const regexHighlight = ({
+    regex = '',
+    type  = 'html' // html | console
+}) => {
+    let err, parsed;
+    const errorRegex = new RegExp(/^SyntaxError:\s+(\/.+?\/)\s+\^\s+Unexpected token:\s+"(.+?)"\s+at\s+(\d+):(\d+)\./, 'm');
+    // because the regexp-tree.js is generated, and I don't feel like 
+    // crawling into the source any more than I already have...
+    // plus hey I'm using regexes for *everything* else, may as well parse
+    // an error.
+    
+    try {
+        parsed = regexpTree.parse(regex, { captureLocations: true });
+    }
+    catch (err)
+    {
+        let matches = errorRegex.exec(err);
+        throw { text: 'Unexpected token in regex parse', pattern: matches[1], token: matches[2], line: matches[3], column: matches[4] };
+    }
+    
+    return parsed;
+    
+};
+
+export default regexHighlight;
 
--- tests/1.js
+++ tests/1.js
@@ -1,8 +1,8 @@
 import test from 'ava';
-import '../regex-highlighter';
+import regexHighlight from '../regex-highlighter';
 
-test('foo', t => {
-  t.pass();
+test('import', t => {
+  t.truthy(regexHighlight);
 });
 
 test('bar', async t => {
@@ -10,3 +10,24 @@ test('bar', async t => {
   t.is(await bar, 'bar');
 });
 
+test('regex parse',  t => {
+   const regexText = regexHighlight({ regex: '/a|b+/' });
+   t.truthy(regexText);
+    
+});
+
+test('regex parse with errors',  t => {
+    try {
+        const regexText = regexHighlight({ regex: '/a|b++/' });
+        // FIXME: this will behave differently if passed as a RegExp object 
+        // (without the quotes)
+    }
+    catch (err) {
+        // console.log(err);
+        t.is(err['text'], 'Unexpected token in regex parse');
+        t.is(err['token'], '+');
+        t.is(err['column'], '5');
+        t.is(err['line'], '1');
+        t.is(err['pattern'], '/a|b++/');
+    }
+});
