8191f72a497c1b1740d802f588b12777afd165fd
Danny Brian <danny@brians.org>
2019-03-26 00:07:29 -0600

    in-progress traversal of AST, but I'm going to refactor this to use the AST tree instead of the visitor patterns provided by regexp-tree...

--- regex-highlighter.js
+++ regex-highlighter.js
@@ -8,39 +8,67 @@ const errorRegex = new RegExp(/^SyntaxError:\s+(\/.+?\/)\s+\^\s+(Unexpected.+?):
 // regexHighlight: ({regex:String, type?:String}) => String
 // IMPORTANT: the regex into needs to be a string
 
-const regexHighlight = ({
+export const regexHighlight = ({
     regex = '',
     type  = 'html' // html | term
 }) => {
-    let err, parsed;
+    
+    let err = {}, ret = {}, ast = {};
     
     if (typeof regex !== 'string') {
-        throw { message: 'Parameter regex must be a string' };    
+        throw new RegexError ('Parameter regex must be a string');    
     }
-    
-    
     
     // FIXME: we need to remove the {{}} syntax and then put it back, while adjusting 
     // the pattern length to accomodate them; kind of a pain.
 
     try {
-        parsed = regexpTree.parse(regex, { captureLocations: true });
+        ast = regexpTree.parse(regex, { captureLocations: true });
     }
     catch (err)
     {
         let matches = errorRegex.exec(err);
         if (matches.length > 0) {
-            throw { message: matches[2], pattern: matches[1], token: matches[3], line: matches[4], column: matches[5] };
+            return { success: false, message: matches[2], pattern: matches[1],
+                     token: matches[3], line: matches[4], column: matches[5] };
         }
         else
         {
-            throw { message: "Invalid regular expression, unknown error" };        
+            throw new RegexError ('Invalid regular expression, unknown error');        
         }
     }
+
+    ret.success = true;
+    let text = '';
+    let textAr = [];
     
-    return parsed;
+    ret.ast = ast;
     
+    // I initially implemented this using regexpTree.traverse, but the visiting 
+    // didn't give me the pattern to properly filter; not everything needs 
+    // labeling, only certain key types (there's nothing to label for Alternative and
+    // Repetition, for example). So it works better to recurse the AST and just 
+    // label the stuff we care about.
+    
+    ret.re = regexpTree.traverse(ast, {
+        Char({node}) {
+            textAr.push("<span class='char " + node.kind + "'>" + (node['escaped'] ? "\\" : "") + node.value + "</span>");
+        },
+        CharacterClass:  {
+            pre({node}) {
+              textAr.push("<span class='charclass " + node.type + "'>[" + (node['negative'] ? "^" : ""));
+            },
+            post({node}) {
+              textAr.push("]</span>");
+            }
+            
+        }
+      },
+    );
+    ret.array = textAr;
+    ret.text = textAr.join('');
+    return ret;
 };
 
-export default regexHighlight;
+export class RegexError extends Error {}
 
--- tests/1.js
+++ tests/1.js
@@ -1,43 +1,69 @@
+const util = require('util')
+
 import test from 'ava';
-import regexHighlight from '../regex-highlighter';
+import { regexHighlight, RegexError } from '../regex-highlighter';
 
 test('import', t => {
   t.truthy(regexHighlight);
 });
 
+/*
 test('bar', async t => {
   const bar = Promise.resolve('bar');
   t.is(await bar, 'bar');
 });
+*/
 
 
 test('regex parse',  t => {
-   const regexText = regexHighlight({ regex: '/a|b+/' });
-   t.truthy(regexText);
+   const regexHL = regexHighlight({ regex: '/a|b+/' });
+   t.truthy(regexHL);
 });
 
 test('passing RegExp object', t => {
     try {
-        const regexText = regexHighlight({ regex: /a|b+/ });
+        const regexHL = regexHighlight({ regex: /a|b+/ });
         // we outlaw this because we want regexHighlight to throw the 
         // errors, rather than JavaScript doing so.
     }
-    catch (err) {
-        t.is(err['message'], 'Parameter regex must be a string');
+    catch (err)
+    {
+        t.truthy(err instanceof RegexError);
+        t.is(err.message, 'Parameter regex must be a string');
     }
 });
 
 test('regex string parse with errors',  t => {
-    try {
-        const regexText = regexHighlight({ regex: '/a|b++/' });
-    }
-    catch (err) {
-        t.is(err['message'], 'Unexpected token');
-        t.is(err['token'], '+');
-        t.is(err['column'], '5');
-        t.is(err['line'], '1');
-        t.is(err['pattern'], '/a|b++/');
-    }
+    // we don't throw exceptions with parse errors, wanting this information
+    // to be handled and displayed by the editor.
+    const regexHL = regexHighlight({ regex: '/a|b++/' });
+    t.false(regexHL.success);
+    t.is(regexHL['message'], 'Unexpected token');
+    t.is(regexHL['token'], '+');
+    t.is(regexHL['column'], '5');
+    t.is(regexHL['line'], '1');
+    t.is(regexHL['pattern'], '/a|b++/');
 });
 
-//var regex1 = '/q(?!u)(\w+)[a-z]{1,}(?<hello>{{hello}})/i';
+test('regex AST', t => {
+    const regexHL = regexHighlight({ regex: '/q(?!u)(\w+)[a-z]{1,}(?<hello>hello)/i' });
+    t.true(regexHL.success);
+    
+    // console.log(util.inspect(regexHL, { showHidden: false, depth: null, colors: true }))
+    
+});
+
+test('AST chars', t => {
+     const regexHL = regexHighlight({ regex: '/\\u003B\\42\\x3Bq\\u{1F680}\\ud83d\\ude80❤️/u' });
+     t.is(regexHL.array[0], "<span class='char unicode'>\\u003B</span>");
+     t.is(regexHL.array[4], "<span class='char unicode'>\\u{1F680}</span>");
+     t.is(regexHL.array[6], "<span class='char simple'>❤</span>");
+     //console.log(regexHL.text);
+
+});
+
+test('AST character class', t => {
+     const regexHL = regexHighlight({ regex: '/[a-z]+[^0-9]*/' });
+     console.log(regexHL.text);
+     console.log(util.inspect(regexHL.ast, { showHidden: false, depth: null, colors: true }));
+});
