e1a0dc19e2fe3710a0881150547dcc7ea1dabbb6
Danny Brian <danny@brians.org>
2019-04-07 23:44:17 -0600

    now as a custom element (let-element); moved index.less to src/

--- src/index.js
+++ src/index.js
@@ -4,35 +4,45 @@ const util = require('util');
 
 import { regexHighlight, RegexError } from '../src/regex-highlighter';
 import ulog from 'ulog';
-
 const log = ulog('regex');
-window.log = log;
 log.level = log.WARN;
-                
-const escapeHTML = (unsafe) => {
-    return unsafe
+
+import { LitElement, html } from 'lit-element';
+
+class RegexEditor extends LitElement {
+    static get properties() {
+        return {
+            parseResult: String,
+            debugLevel: String,
+            regexValue: { attribute: true, reflect: true }
+        };
+    }
+    constructor() {
+        super();
+        this.parseResult = 'failure';
+        this.regexValue = 'a|b';
+        this.regexError = '';
+        this.debugLevel = '';
+    }
+    firstUpdated() {
+        this.regexInputEl = this.shadowRoot.getElementById('regexInput');
+        this.regexForeground = this.shadowRoot.getElementById('regexForeground');
+        this.regexBackground = this.shadowRoot.getElementById('regexBackground');
+        // ^^ yes we do need these to make unfortunate/constant innerHTML writes,
+        // see the note at the bottom of src/regex-highlighter.js.
+        this._handleInput();
+    }
+    _escapeHTML(unsafe) {
+        return unsafe
          .replace(/&/g, "&amp;")
          .replace(/</g, "&lt;")
          .replace(/>/g, "&gt;")
          .replace(/"/g, "&quot;")
          .replace(/'/g, "&#039;");
-};
-
-const regexInput = document.getElementById('regexInput');
-const regexBackground = document.getElementById('regexBackground');
-const regexForeground = document.getElementById('regexForeground');
-const regexContainer = document.getElementsByClassName('regexContainer')[0];
-const regexError = document.getElementsByClassName('regexError')[0];
-
-regexContainer.classList.remove('success');
-
-const showError = (e) => {
-    regexError.textContent = e;
-};
-
-const inputChange = (e) => {
-    setTimeout(function() {
-        let regexVal = regexInput.value; // the raw input, which we don't change
+    }
+    _handleInput(e) {
+        let regexVal = this.regexInputEl.value; // the raw input, which we don't change
+        this.regexValue = regexVal; // reflected to property
         try
         {
             let regex = regexHighlight({ regex: '/' + regexVal + '/' });
@@ -43,11 +53,11 @@ const inputChange = (e) => {
                 if (regex.html === undefined) {
                     log.error('Regex HTML is undefined, but saying success?');
                 }
-                regexContainer.classList.add('success');
-                showError('');
+                this.parseResult = 'success';
+                this.regexError = '';
                 let escapedHTMLVal = regex.html;
-                regexBackground.innerHTML = escapedHTMLVal;
-                regexForeground.innerHTML = escapedHTMLVal;
+                this.regexBackground.innerHTML = escapedHTMLVal;
+                this.regexForeground.innerHTML = escapedHTMLVal;
             }
             else // what do we do here?
             {
@@ -55,30 +65,52 @@ const inputChange = (e) => {
             }
         }
         catch (err) {
-            regexContainer.classList.remove('success');
+            this.parseResult = 'failure';
             log.debug('Regex error caught: ' + JSON.stringify(err));
             if (err['error']) {
                 if (err.error['message'] && err.error['token']) {
-                    showError(err.error.message + `: '${err.error.token}'.`);
+                    this.regexError = '⚠ '+ err.error.message + `: '${err.error.token}'.`;
                     log.debug('Regex parse error: ' + JSON.stringify(err));
                 }
             }
             else if (err['message']) 
             {
-                showError(err.message);
+                this.regexError = err.message;
             }
             else
             {
                 log.debug('Regex error caught but no information to display.');
             }
             // use whatever is in the text area input value
-            let escapedHTMLVal = escapeHTML(regexVal);
-            regexBackground.innerHTML = escapedHTMLVal;
-            regexForeground.innerHTML = escapedHTMLVal;
+            let escapedHTMLVal = this._escapeHTML(regexVal);
+            this.regexBackground.innerHTML = escapedHTMLVal;
+            this.regexForeground.innerHTML = escapedHTMLVal;
         }
-    }, 0);
-};
+    }
+    render(){
+        // FIXME: instead of loading CSS here, use static styles etc.
+        // https://lit-element.polymer-project.org/guide/styles
+        // NOTE: this HTML can easily be used outside of Polymer/lit-element
+        // in an HTML file and converting the JS to vanilla JS. I'm mindful
+        // of the rapid changes still occuring with Polymer.
+        return html`
+          <link rel="stylesheet" href="regex-editor-bundle.css">
+          <div id="regexTopContainer">
+            <div id="regexPrefix">/</div>
+            <div id="regexSuffix">/</div>
+            <div id="regexContainer" class="${this.parseResult}">
+                <div id="regexBackground"></div>
+                <div id="regexInputContainer">
+                    <textarea id="regexInput" @input="${this._handleInput}" @blur="${this._handleInput}" wrap="hard" placeholder="regex goes here" autocorrect="off" autocapitalize="off" autocomplete="off" spellcheck="false" scrolling="no">${this.regexValue}</textarea>
+                </div>
+                <div id="regexForeground"></div>
+            </div>
+            <div id="regexError">${this.regexError}</div>
+          </div>
+        `;
+        // ANOTHER NOTE: There's a lot of custom stuff going on with the events
+        // trigged by <textarea>, including rewriting of its value on errors etc.
+    }
+}
 
-regexInput.addEventListener('input', inputChange);
-regexInput.addEventListener('blur', inputChange);
-
+customElements.define('regex-editor', RegexEditor);
